46b21c0a788e0a9e803b294fd0d6374d48b4ce91
pruizfer <pichininiruizfernando@gmail.com>
2024-08-19 13:40:09 -0300

    Delete pruizf.github.io directory

--- pruizf.github.io/YT/help/return.js
+++
@@ -1,8 +0,0 @@
-function onReturnClick() {
-    open("./", "_self")
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('#returnbutton')
-    .addEventListener("click", onReturnClick)
-})
